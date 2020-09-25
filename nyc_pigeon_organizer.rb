c458a2158c740405d73b2c42c261f4ebbd5a35f0
Jesse Vaughn <jcv221@gmail.com>
2020-09-25 23:03:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,10 +8,9 @@ def nyc_pigeon_organizer(data)
           birds_new[name] = {}
         end
         if !birds_new[name][key]
-          birds_new[name][key] = [colors]
+          birds_new[name][key] = [colors] << colors.to_s
           binding.pry
       end 
-      birds_new
     end 
    end  
 end
